f0a64e11d233497a154b7ab5a97e473011b94f0a
radu@irk <radu@irk>
2012-04-03 23:10:04 +0300

    renders to immage

--- src/buddah.go
+++ src/buddah.go
@@ -2,41 +2,69 @@ package main
 
 import (
 	"fmt"
+	"image"
+	"image/color"
+	"image/png"
+	"os"
+	"runtime"
 )
 
 const size = 128
-const max_iter = 1
+const max_iter = 10000
 
 func square(x int, y int) (int, int) {
 	return (x*x - y*y), (2*x*y)
 }
 
-func mandelbrot(x int, y int) int {
-	// c := complex(float64(x), float64(y))
-	o_x := x
-	o_y := y
-	i:=0
-	for (i<max_iter && (x*x)+(y*y)<4) {
+
+func to_img(brot [size][size]uint) {
+	max := uint(0)
+	gray := image.NewGray16(image.Rect(0,0,size,size))
+	for x:=0; x<size; x++ {
+		for y:=0; y<size; y++ {
+			if brot[x][y] > max{
+				max = brot[x][y]
+			}
+			if brot[x][y] >= max_iter{
+				gray.SetGray16(x, y, color.Black)
+			} else {
+				gray.SetGray16(
+					x, y,
+					color.Gray16{uint16(brot[x][y]*200)})
+			}
+		}
+	}
+	fmt.Printf("%v", max)
+	w, _ := os.OpenFile("./brot.png", os.O_CREATE|os.O_WRONLY, 0666)
+	png.Encode(w, gray)
+}
+
+
+
+func mandelbrot(x int, y int) uint {
+	c := complex(float64(x)/size, float64(y)/size)
+	z := c
+	var i = uint(0)
+	for (i<max_iter && real(z)+imag(z)<4) {
 		i++
-		x, y = square(x, y)
-		x += o_x
-		y += o_y
+		z = z*z + c
 	}
 	return i
 }
 
+// setup 4 goroutines all consuming from one queue
+// all 4 goroutines will send one message when they finish on a second queue
+
 func main() {
-	var brot [size][size]bool
+	runtime.GOMAXPROCS(4)
+	var brot [size][size]uint
+	// in := make(chan image.Point)
+	// finished := make(chan bool)
 	for x:=0; x<size; x++ {
+		fmt.Print(".")
 		for y:=0; y<size; y++ {
-			p := mandelbrot(x, y)
-			if p>0 {
-				brot[x][y] = true
-				fmt.Print(".")
-			} else {
-				fmt.Print(" ")
-			}
+			brot[x][y] = mandelbrot(x, y)
 		}
-		fmt.Print("\n")
 	}
+	to_img(brot)
 }
